35cbbaca9fb1d16fd8490e49229072b2bd8f7fc7
Cristhiam Quiñonez Especialista en Marketing Digital | Estrategias de Crecimiento y SEO <chrisquionez354@gmail.com>
2025-04-12 22:04:17 -0600

    Create node.py
    [Instrucciones...]

---
+++ core/node.py
@@ -0,0 +1,26 @@
+[from setuptools import setup, find_packages 
+
+setup(
+    name="hormigasais-seopack",
+    version="0.1.0",
+    author="Cristhiam Quiñonez",
+    author_email="hola@hormigasais.com",
+    description="Paquete SEO ligero y automatizable de TheAntsMind, desarrollado por HormigasAIS.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheAntsMind/TheAntsMind",  # Reemplazá con el link del repo final
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
+]
